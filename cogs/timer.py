62f28073749eb99358a70f2efc83a0c43900941f
peaches <peizhe2001@hotmail.com>
2024-05-28 21:25:07 -0400

    add comments and organize code

--- cogs/timer.py
+++ cogs/timer.py
@@ -3,49 +3,45 @@ from discord import SelectOption, ui, Button, ButtonStyle, Interaction
 import asyncio
 from typing import Final
 
+### seconds class ###
 class sDropdown(ui.Select):
     def __init__(self) -> None:
         options: Final[list[SelectOption]] = []
         for i in range (60):
             if i % 15 == 0:
-                options.append(SelectOption(label=f"{i}", value=f"{i}"))
-        print(options)
-
+                options.append(SelectOption(label=f"{i}", value=f"{i}", default=(i==0)))
         super().__init__(placeholder="Seconds", options=options, custom_id="selectSeconds", min_values=1, max_values=1)
 
     async def callback(self, interaction: Interaction) -> None:
         self.view.seconds = int(self.values[0])
         await interaction.response.defer()
 
-
+### minutes class ###
 class mDropdown(ui.Select):
     def __init__(self) -> None:
         options: Final[list[SelectOption]] = []
         for i in range (60):
             if i % 15 == 0:
-                options.append(SelectOption(label=f"{i}", value=f"{i}"))
-        print(options)
-            
+                options.append(SelectOption(label=f"{i}", value=f"{i}", default=(i==0)))
         super().__init__(placeholder="Minutes", options=options, custom_id="selectMinutes", min_values=1, max_values=1)
 
     async def callback(self, interaction: Interaction) -> None:
         self.view.minutes = int(self.values[0])
         await interaction.response.defer()
 
-
+### hours class ###
 class hDropdown(ui.Select):
     def __init__(self) -> None:
         options: Final[list[SelectOption]] = []
         for i in range (5):
-            options.append(SelectOption(label=f"{i}", value=f"{i}"))
-        print(options)
-            
-        super().__init__(placeholder="Hours", options=options, custom_id="selectHours", min_values=1, max_values=1)   
+            options.append(SelectOption(label=f"{i}", value=f"{i}", default=(i==0)))
+        super().__init__(placeholder="Hours", options=options, custom_id="selectHours", min_values=1, max_values=1)
 
     async def callback(self, interaction: Interaction) -> None:
         self.view.hours = int(self.values[0])
         await interaction.response.defer()
 
+### confirm button class ###
 class ConfirmButton(ui.Button):
     def __init__(self):
         super().__init__(label="Confirm", style=ButtonStyle.green, custom_id="confirmButton")
@@ -57,16 +53,16 @@ class ConfirmButton(ui.Button):
         else:
             await interaction.response.send_message(f"Timer set for {total_seconds} seconds. I'll remind you!", ephemeral=True)
             await asyncio.sleep(total_seconds)
-            await interaction.followup.send("Time's up! gimme your money")
+            await interaction.followup.send("Time's up! gimme your money", ephemeral=True)
 
-
+### Viewing class ###
 class TimerView(ui.View):
     def __init__(self) -> None:
         super().__init__()
         self.seconds = 0
         self.minutes = 0
         self.hours = 0
-        self.add_item(hDropdown())
+        self.add_item(hDropdown()) 
         self.add_item(mDropdown())
         self.add_item(sDropdown())
         self.add_item(ConfirmButton())
@@ -75,7 +71,7 @@ class TimerView(ui.View):
         # Allow only the user who initiated the command to interact
         return interaction.user == interaction.message.interaction.user
 
-
+### Timer command class ###
 class Timer(commands.Cog):
     def __init__(self, client: commands.Bot) -> None:
         self.client= client
@@ -90,12 +86,12 @@ class Timer(commands.Cog):
 
         total_seconds = await view.calculate_total_seconds()
         if total_seconds <= 0:
-            await ctx.send("You need to set a time greater than 0 seconds.")
+            await ctx.send("You need to set a time greater than 0 seconds.", ephemeral=True)
             return
 
-        await ctx.send(f"Timer set for {total_seconds} seconds. I'll remind you!")
+        await ctx.send(f"Timer set for {total_seconds} seconds. I'll remind you!", ephemeral=True)
         await asyncio.sleep(total_seconds)
-        await ctx.send(f"Time's up! gimme your money")
+        await ctx.send(f"Time's up! gimme your money", ephemeral=True)
 
 
 async def setup(client: commands.Bot):
