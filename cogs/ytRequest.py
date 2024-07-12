d7290d9a0d1b40c586193ad7cce5a758d30b2fc6
peaches <peizhe2001@hotmail.com>
2024-07-11 21:38:31 -0400

    include options in the DURATION if statement

--- cogs/ytRequest.py
+++ cogs/ytRequest.py
@@ -4,25 +4,25 @@ import yt_dlp
 def audioDownloadYT(
     URL: str, OUTPUT_PATH: str, DURATION: int, START_TIME: int = 0
 ) -> None:
-    options = {
-        "format": "bestaudio/best",
-        "outtmpl": OUTPUT_PATH,
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-        "postprocessor_args": [
-            "-ss",
-            str(START_TIME),  # Start time
-            "-t",
-            str(DURATION),  # Duration of the clip
-        ],
-    }
+    if DURATION <= 15:
+        options = {
+            "format": "bestaudio/best",
+            "outtmpl": OUTPUT_PATH,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "postprocessor_args": [
+                "-ss",
+                str(START_TIME),  # Start time
+                "-t",
+                str(DURATION),  # Duration of the clip
+            ],
+        }
 
-    if DURATION <= 30:
         with yt_dlp.YoutubeDL(options) as ydl:
             ydl.download([URL])
     else:
